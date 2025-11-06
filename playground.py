0e0cc8655369b86aaabf78adf78fefc337d934dc
Ashwath S <miniash123@outlook.com>
2025-11-06 20:45:49 +0530

    kwargs intro logic

--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 def add(*args):
+    print(args) #tuple
     sum = 0
 
     for num in args:
@@ -6,5 +7,27 @@ def add(*args):
 
     return sum
 
+def calculate(n, **kwargs):
+    print(kwargs) #dictionary+
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    return n
+
 
 print(add(45,46,23,123, 1, 2, 3, 4))
+
+print(calculate(2, add=3, multiply=5))
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Ford")
+print(my_car.make)
+print(my_car.model)
